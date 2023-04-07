b377ada35ae2a25109ad0eccb2d645f069c14679
gabusy07 <jin_thecraziest@live.com>
2023-04-06 21:34:10 -0300

    create: init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './layout/home/home.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { InfobarComponent } from './component/infobar/infobar.component';
+import { StartMainComponent } from './component/start-main/start-main.component';
+import { RegisterFormComponent } from './layout/register-form/register-form.component';
+import { LoginFormComponent } from './layout/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    InfobarComponent,
+    StartMainComponent,
+    RegisterFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
