4da223e2c0b8c4789ccd49cf9e2fdee307efa75d
gabusy07 <jin_thecraziest@live.com>
2023-04-06 22:02:38 -0300

    add: material module

---
+++ src/app/module/material/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatChipsModule} from '@angular/material/chips';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ], exports: [
+    MatChipsModule
+  ]
+})
+export class MaterialModule { }
