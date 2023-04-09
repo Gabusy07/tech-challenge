819dd9a93b88f364c1dcb399c2937966c1491af2
gabusy07 <jin_thecraziest@live.com>
2023-04-09 19:50:08 -0300

    fix: wallpaper

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
+
+  public form:FormGroup
+
+  constructor(private readonly formBuilder : FormBuilder) {
+    this.form = this.initForm();
+    
+   }
+
+  ngOnInit(): void {
+    
+  }
+
+  //construccion del reactiveForm
+  private initForm(): FormGroup{
+    return this.formBuilder.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(18)]],
+    })
+
+  }
+
+  get Email(){
+    return this.form.get('email');
+  }
+
+  get Password(){
+    return this.form.get('password');
+  }
 
 }
--- src/app/auth/register-form/register-form.component.ts
+++ src/app/auth/register-form/register-form.component.ts
@@ -8,21 +8,34 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterFormComponent {
 
-  formulario!: FormGroup;
+  form!: FormGroup;
 
   constructor(private fb:FormBuilder){
   }
 
   ngOnInit() {
-    this.formulario = this.fb.group({
-      nombre: ['', Validators.required],
+    this.form = this.fb.group({
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      mensaje: ['', Validators.required]
+      password: ['', Validators.required, Validators.minLength(5), Validators.maxLength(18)]
     });
   }
 
+  get Name(){
+    return this.form.get('name');
+  }
+
+
+  get Email(){
+    return this.form.get('email');
+  }
+
+  get Password(){
+    return this.form.get('password');
+  }
+
   enviar() {
-    if (this.formulario.valid) {
+    if (this.form.valid) {
       // Procesar el formulario aquí
     }
   }
