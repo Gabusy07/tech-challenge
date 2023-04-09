02be91e28380851e1ca8f4938fb842af854fe36c
gabusy07 <jin_thecraziest@live.com>
2023-04-09 19:54:47 -0300

    fix: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
 import { WelcomePageComponent } from './layout/welcome-page/welcome-page.component';
+import { LoginFormComponent } from './auth/login-form/login-form.component';
+import { RegisterFormComponent } from './auth/register-form/register-form.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'signin', component: RegisterFormComponent }
 ];
 
 @NgModule({
